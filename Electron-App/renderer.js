da1523ad67126e4eb34653293cfe8c7c1cbd9f64
Ryan Olsen <rol4400@outlook.com>
2025-11-06 14:23:24 +1000

    Adjusting mac pen position 6

--- Electron-App/renderer.js
+++ Electron-App/renderer.js
@@ -37,6 +37,7 @@ function positionPenIcon() {
     toolbarContainer.style.bottom = `${bottomOffset}px`;
     
     // Update color picker position if toolbar is visible
+    updateColorPickerPosition();
     if (isEnabled) {
         setTimeout(updateColorPickerPosition, 50);
     }
@@ -228,6 +229,7 @@ function openPen() {
         <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
     </svg>`;
     
+    updateColorPickerPosition();
     setTimeout(() => {
         updateColorPickerPosition();
         colorPicker.style.pointerEvents = 'auto';
